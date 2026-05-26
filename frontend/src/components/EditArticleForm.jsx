f26d5de37ced327038b9ddb2f6439f43f1f9f930
P SHIVA KUMAR <23eg112e50@anurag.edu.in>
2026-05-26 10:41:57 +0530

    Update EditArticleForm.jsx

--- frontend/src/components/EditArticleForm.jsx
+++ frontend/src/components/EditArticleForm.jsx
@@ -41,7 +41,7 @@ function EditArticle() {
   const updateArticle = async (data) => {
     console.log(data);
     data.articleId = article._id;
-    let res = await axios.put("https://final-blog-app.onrender.com/author-api/articles", data, { withCredentials: true });
+    let res = await axios.put("https://final-blog-01.onrender.com/author-api/articles", data, { withCredentials: true });
     console.log("res update atricle", res);
     navigate(`/article/${article._id}`, {
       state: res.data.payload,
